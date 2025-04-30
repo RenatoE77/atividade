84bc6a755a6d15c054c8114c54958992445c1939
renato <renatoeveraldo907@gmail.com>
2025-04-30 16:10:32 -0300

    .

---
+++ atividade_3/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        FakerFactory::create(10)->unique(true);
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
+        ]);
+        // User::factory(10)->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
